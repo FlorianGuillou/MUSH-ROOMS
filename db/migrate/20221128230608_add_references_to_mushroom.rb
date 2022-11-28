87a2b15c8def04d8000167aaa6f69012bfbf3f81
Florian Guillou <guilflo@hotmail.fr>
2022-11-28 17:42:41 -0600

    model done except reviews

--- db/migrate/20221128230608_add_references_to_mushroom.rb
+++ db/migrate/20221128230608_add_references_to_mushroom.rb
@@ -1,5 +1,5 @@
 class AddReferencesToMushroom < ActiveRecord::Migration[7.0]
   def change
-    add_column :mushrooms, :user, :references, foreign_key: true
+    add_column :mushrooms, :user_id, :integer, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_230608) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_234134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_230608) do
     t.bigint "mushroom_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["mushroom_id"], name: "index_bookings_on_mushroom_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "mushrooms", force: :cascade do |t|
@@ -64,11 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_230608) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "mushrooms"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -83,4 +82,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_230608) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "mushrooms"
+  add_foreign_key "bookings", "users"
 end
