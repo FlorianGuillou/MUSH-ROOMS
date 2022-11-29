8f4f04ff487c6e05934f00d7458718fae9ed59e2
Florian Guillou <guilflo@hotmail.fr>
2022-11-29 14:12:42 -0600

    routes mushrooms done + font awesome add

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -2,4 +2,37 @@ class MushroomsController < ApplicationController
   def show
     @mushroom = Mushroom.find(params[:id])
   end
+
+  def new
+    @mushroom = Mushroom.new
+  end
+
+  def create
+    @mushroom = Mushroom.new(mushroom_params)
+    if @mushroom.save
+      redirect_to root_path, notice: 'MushRoom was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    raise
+  end
+
+  def update
+    raise
+  end
+
+  def destroy
+    @mushroom = Mushroom.find(params[:id])
+    @mushroom.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def mushroom_params
+    params.require(:mushroom).permit(:category, :color, :price, :description, :photo, :address, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :mushrooms, only: [:show]
+  resources :mushrooms, except: [:index]
 end
