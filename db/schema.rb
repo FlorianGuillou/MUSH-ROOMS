762b4dfc67e7df22212de3175122b426f350c8d7
Marjorie MONDON <marjmondon@hotmail.com>
2022-11-29 12:42:28 -0500

    ajout de avatar description et name pour la page registration pour user / devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_234134) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_162542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_234134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "description"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
