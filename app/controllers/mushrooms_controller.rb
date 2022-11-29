f3e41383a12742005a451738c3974df7f005f9f2
Lamia Belkacem <lamia.belkacem86@gmail.com>
2022-11-29 16:01:41 -0500

    ligne vide supprimee

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -1,7 +1,6 @@
 class MushroomsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
-
   def show
     @mushroom = Mushroom.find(params[:id])
     authorize @mushroom
