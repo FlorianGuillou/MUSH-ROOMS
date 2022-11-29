bac28275e18e48624b6bdc13e79d6a014b72c550
Marjorie MONDON <marjmondon@hotmail.com>
2022-11-29 15:28:54 -0500

    seed pour user et mushroom

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 puts 'Clear DB..'
 
 Mushroom.destroy_all
+User.destroy_all
+
+puts 'Create data...'
+greedy = User.create!(name: "Greedy", email: "greedy@smufrs.com", password: "12345678", avatar: "smurf.png")
+grand = User.create!(name: "Grand", email: "grand@smufrs.com", password: "12345678", avatar: "smurf.png")
+puts "Done user"
 
 puts 'Create data...'
 name = ['Weird', 'Amazing', 'Awesome', 'Dark', 'Death']
@@ -13,9 +19,10 @@ price = [*(10..100)]
     name: name.sample,
     category: category.sample,
     color: color.sample,
-    price: price.sample
+    price: price.sample,
+    user_id: greedy.id
   )
   mushroom.save!
 end
 
-puts 'Done!'
+puts 'Done pour Mushroom!'
