beacb625dc06b87fcfcc76a047d419c8411d61cf
Marc andre camirand <camirand.mac@gmail.com>
2022-12-02 10:01:44 -0500

    added sql to controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if params[:query].present?
+      sql_query = <<~SQL
+      mushrooms.name @@ :query
+    SQL
+      @mushrooms = Mushroom.where(sql_query, query: "%#{params[:query]}%")
+    else
     @mushrooms = policy_scope(Mushroom)
+    end
   end
 end
