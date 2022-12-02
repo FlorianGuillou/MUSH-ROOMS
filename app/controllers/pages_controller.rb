7de8037dc6f4ecdb0519f90434ea31f380006a01
Marc andre camirand <camirand.mac@gmail.com>
2022-12-02 10:46:24 -0500

    added style for search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,20 @@ class PagesController < ApplicationController
   def home
     if params[:query].present?
       sql_query = <<~SQL
-      mushrooms.name @@ :query
+      mushrooms.name ILIKE :query
     SQL
       @mushrooms = Mushroom.where(sql_query, query: "%#{params[:query]}%")
     else
     @mushrooms = policy_scope(Mushroom)
     end
+
+    # if params[:eatable].present?
+    #   sql_eatable = <<~SQL
+    #   mushrooms.eatable ILIKE :eatable
+    # SQL
+    #   @mushrooms = Mushroom.where(sql_eatable, eatable: "%#{params[:eatable]}%")
+    # else
+    #   @mushrooms = policy_scope(Mushroom)
+    # end
   end
 end
